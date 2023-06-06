8753e5b4d4b364490192d4bcdd58caa91cd53db7
mmg114 <Mauricio Munoz Guzman>
2023-06-05 19:45:43 -0500

    Trabajo y clases de Estructuras if and for

---
+++ src/main/java/co/com/repuestos/ps/util/Util.java
@@ -0,0 +1,21 @@
+package co.com.repuestos.ps.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Util {
+
+    public static void main(String[] args) {
+        //TODO  Ejercio1 Hacer un menu de restaurant  switch ();
+
+
+
+        //TODO  Ejercio2
+
+
+        //TODO  Ejercio3 crear una lista con numero repetidos"Al menos 2" y encontrarlos.
+
+
+
+    }
+}
